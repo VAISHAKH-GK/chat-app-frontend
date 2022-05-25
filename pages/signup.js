6a0e90dce458a79e8b3aa45972c7051ef1e660b1
VAISHAKH GK <vaishakhgk2006@gmail.com>
2022-05-25 12:38:53 +0530

    signup function added

--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import styles from '../styles/signup.module.css';
+import axios from 'axios';  
 
 const Signup = () => {
   
   const [userName,setUserName] = useState();
   const [password,setPassword] = useState();
+  const [user,setUser] = useState(null);
+
+  const doSignUp = async () => {
+    const data = {
+      userName,
+      password
+    }
+    axios.post('http://localhost:9000/api/signup',data).then((response) => {
+      if (!response.data.success) console.log(response.data.reason) ;
+      else setUser(response?.data?.user);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  useEffect(() => {
+    console.log(user);
+  },[user]);
 
   return (
     <div className={styles.body} >
@@ -19,7 +38,7 @@ const Signup = () => {
           <input type="password" value={password} onChange={ e => setPassword(e.target.value) } placeholder="Password"/>
         </div>
         <div id={styles.button} className={styles.row}>
-          <button>SignUp</button>
+          <button onClick={doSignUp} >SignUp</button>
         </div>
       </div>
     </div>
