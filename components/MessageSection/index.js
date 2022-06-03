1ad34370a70fd7e9cb756aeef8714cd88c06d25e
VAISHAKH GK <vaishakhgk2006@gmail.com>
2022-06-03 23:12:14 +0530

    textarea added and styled

--- components/MessageSection/index.js
+++ components/MessageSection/index.js
@@ -15,10 +15,39 @@ export default function ChatSection () {
     )
   }
 
+  const MessagesArea = () => {
+    return (
+      <div className={`${styles.messagesArea}`} >
+      {
+        dmUser ? <h1>Messages</h1> : <h1>Home</h1>
+      }
+      </div>
+    )
+  }
+
+  const TextInput = () => {
+    return (
+      <div className={`${styles.textInput}`} >
+        <textarea className={`${styles.textarea}`} onInput={autoHeight}  placeholder="Type Message ..." ></textarea>
+        <button className={`btn btn-success ${styles.sendButton}`}  >Send</button>
+      </div>
+    )
+  }
+
+  function autoHeight( e ) {
+    const textarea = e.target;
+    textarea.style.height = "auto";
+    textarea.style.height = (textarea.scrollHeight) + "px";
+  }
+
   return (
     <div className={`${styles.chatSection}`} >
       <div className={`${styles.main}`} >
         <Header/>
+        <MessagesArea/>
+        {
+          dmUser ? <TextInput/> : ""
+        } 
       </div>
     </div>
   )
